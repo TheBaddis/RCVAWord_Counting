60a7fc5931dc5bc7a6cb5e1bf91a73a2bb6f043e
CarlHagglund <calle.hagglund@gmail.com>
2025-09-24 16:21:48 +0200

    commit test från terminal

---
+++ wordfreq.py
@@ -0,0 +1,17 @@
+def tokenize(documentWithTextLines):
+    words = []
+
+
+    for line in documentWithTextLines:
+        index = 0
+
+        print(f"nu kommer raden:{line}")
+
+        while (index < len(line)):
+            print(line[index])
+            index += 1
+    
+    return words
+
+
+tokenize(["första rad", "andra rad"])
